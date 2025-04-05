02ad6f6fc043fa0a8f99a4f342c6017f915dbde6
Daria891719 <dariazdorik@gmail.com>
2025-04-05 10:03:42 +0000

    new code

--- in-class-exercises/lesson-5/exercise-pt1.py
+++ in-class-exercises/lesson-5/exercise-pt1.py
@@ -7,8 +7,9 @@ Write a function `greeting` that takes 2 arguments (first_name and last_name) an
 
 # solution for exercise 1 👇🏽
 
-
-
+def greeting(first_name, last_name):
+    print ("Hello, ",first_name, last_name,", are you ready for some fun coding today?")
+greeting ("Alice", "Smith")
 
 '''
 EXERCISE 2
@@ -20,8 +21,12 @@ For example, if the character is 'X' and the number is 4, the function should re
 '''
 
 # solution for exercise 2 👇🏽
-
-
+def repeat_character (character, repeat_times):
+#    list=[]
+#    if x<=number:
+#      list_fin= list.append(character)
+    print(character*repeat_times)
+print(repeat_character ("Z",10))
 
 '''
 EXERCISE 3
@@ -29,5 +34,14 @@ EXERCISE 3
 Copy and paste your solution from exercise 2. 
 This time, modify the function so that, if the given number is bigger than 10, it will print out "Sorry, too long!"
 '''
+
+def repeat_character (character, repeat_times):
+    if repeat_times<=10:
+        print(character*repeat_times)
+    else:
+        print ("Sorry, too long!")
+print(repeat_character ("Z",11))
+
+
 
 # solution for exercise 3 👇🏽
